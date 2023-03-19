a6320051292156bbe05de829e01bc757cf58237a
Andrey <druss25@yandex.ru>
2023-03-19 15:48:18 +0500

    Fixed ...

--- gulp/tasks/reset.js
+++ gulp/tasks/reset.js
@@ -1,6 +1,6 @@
 import clean from "gulp-clean";
 
 export const reset = () => {
-	return app.gulp.src(app.path.clean, {})
+	return app.gulp.src(app.path.clean, {allowEmpty: true})
 			.pipe(clean())
 }
