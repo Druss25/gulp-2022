6f53bef93fec64976723a327d05592582680c13d
Andrey Tatsiy <druss25@yandex.ru>
2023-03-20 21:17:51 +0500

    Delete scss/_libs and fonts.scss

--- gulp/tasks/css.js
+++ gulp/tasks/css.js
@@ -1,6 +1,6 @@
 
 export const css = () => {
-  return app.gulp.src(['node_modules/normalize.css/normalize.css','node_modules/swiper/swiper.css'])
+  return app.gulp.src(['node_modules/normalize.css/normalize.css','!node_modules/swiper/swiper.css'])
     .pipe(app.plugins.concat(`_libs.scss`))
     .pipe(app.gulp.dest(app.path.src.lib))
 }
